dbbc4639771859d83ecbf7023a6bc49ad46fc834
Dogu <dogudeniz.ugur@gmail.com>
2021-07-11 03:17:54 +0300

    leaderboard

--- Screens/Main/Friends/Advice.js
+++ Screens/Main/Friends/Advice.js
@@ -1,63 +1,78 @@
 import React, {Component} from 'react';
 import {SafeAreaView, StyleSheet, View, ScrollView} from 'react-native';
 import {
-    Divider,
-    Layout,
-    Text,
-    TopNavigation,
-    Icon,
-    TopNavigationAction, Card,
+  Divider,
+  Layout,
+  Text,
+  TopNavigation,
+  Icon,
+  TopNavigationAction,
 } from '@ui-kitten/components';
-import styles from "../../../src/styles";
-import {
-    FriendWarningCard,
-} from '../../../Components/Card';
+import styles from '../../../src/styles';
 
+import Leaderboard from 'react-native-leaderboard';
 
 export default class Advice extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            refreshing: false, name: "", target: 0, stored: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+      name: '',
+      target: 0,
+      stored: 0,
+    };
+  }
 
-        };
-    }
+  componentDidMount() {}
 
-    componentDidMount() {
-    }
+  GoBackIcon = props => <Icon {...props} name="arrow-back-outline" />;
 
-    GoBackIcon = props => <Icon {...props} name="arrow-back-outline"/>;
+  renderLeftActions = () => (
+    <React.Fragment>
+      <TopNavigationAction
+        icon={this.GoBackIcon}
+        onPress={() => this.props.navigation.goBack()}
+      />
+    </React.Fragment>
+  );
 
-    renderLeftActions = () => (
-        <React.Fragment>
-            <TopNavigationAction
-                icon={this.GoBackIcon}
-                onPress={() => this.props.navigation.goBack()}
-            />
-        </React.Fragment>
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <TopNavigation
+          title={<Text style={styles.miniTitle}>Lider Tablosu</Text>}
+          alignment="center"
+          accessoryLeft={this.renderLeftActions}
+        />
+        <Divider />
+
+        <Leaderboard
+          data={[
+            {
+              userName: 'Dogu',
+              km: 50,
+              img:
+                'https://project-lyda.s3.eu-central-1.amazonaws.com/dogu.jpeg',
+            },
+            {
+              userName: 'Ada',
+              km: 120,
+              img:
+                'https://project-lyda.s3.eu-central-1.amazonaws.com/ada.jpeg',
+            },
+            {
+              userName: 'Cinar',
+              km: 100,
+              img:
+                'https://project-lyda.s3.eu-central-1.amazonaws.com/cinar.jpg',
+            },
+          ]}
+          sortBy="km"
+          labelBy="userName"
+          icon="img"
+          style={{backgroundColor: '#000', width: 100}}
+        />
+      </SafeAreaView>
     );
-
-    render() {
-        return (
-            <SafeAreaView style={{flex: 1}}>
-                <TopNavigation
-                    title={<Text style={styles.miniTitle}>Birikim Onerileri</Text>}
-                    alignment="center"
-                    accessoryLeft={this.renderLeftActions}
-                />
-                <Divider/>
-                <Layout style={styles.layout}>
-                    <ScrollView style={styles.container}>
-                        <View style={{width: '100%', paddingHorizontal: 16}}>
-                            <FriendWarningCard
-                                text={"Ev dışındaki harcamaların arkdaşlarindan %35 daha fazla. Bu harcamalardan kısarak birikimlerine odaklanabilirsin."}/>
-                            <FriendWarningCard
-                                text={"Senin yas grubundaki gencler kozmetik harcamalarina aylik olarak %10 daha az harciyorlar. Kayak tatilin icin bu parayi biriktirebilirsin."}/>
-                        </View>
-                        <View style={{height: 75}}/>
-                    </ScrollView>
-                </Layout>
-            </SafeAreaView>
-        );
-    }
+  }
 }
